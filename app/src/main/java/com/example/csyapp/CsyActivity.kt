c98d03cbd999946981a67eb93194d7cb75fbd4d0
csyhlim <caseyhlimx@gmail.com>
2025-05-26 19:54:44 +0700

    casey

---
+++ app/src/main/java/com/example/csyapp/CsyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.csyapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CsyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_csy)
+    }
+}
